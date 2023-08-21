7c76e3223379aa3aec9e56e3c2a7ffb5a1c167dc
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-08-20 22:49:14 -0300

    Barrel status screen added

---
+++ src/screens/barrels/BarrelStatus.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BarrelStatus = () => {
+  return (
+    <View>
+      <Text>BarrelStatus</Text>
+    </View>
+  )
+}
+
+export default BarrelStatus
